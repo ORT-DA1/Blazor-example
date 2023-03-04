a4c63c2e2e096b20065bde7ddbca51ca35dcf7c3
joaquin03 <janduano@gmail.com>
2023-03-03 21:08:27 -0600

    add relation Movie-Category Edit

--- Data/Movie.cs
+++ Data/Movie.cs
@@ -7,6 +7,8 @@ public class Movie
    public string? Director { get; set; }
    public int? ReleaseYear { get; set; }
 
+   public Category Category { get; set; }
+
    public override string ToString()
    {
        return $"Id: {Id}, Title: {Title}, Director: {Director}, ReleaseYear: {ReleaseYear}";
